6ee946e9819336e8f44a2e1174e98fae81f7ae0f
JCJord <julinnn4k@live.com>
2021-09-30 21:39:14 -0300

    fix: prevent scroll when sidenav is open

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,53 +1,59 @@
-/* Set the width of the side navigation to 100% */
 function toggleNav() {
-  let mySidenav = document.getElementById("mySidenav")
+  const mySidenav = document.getElementById("mySidenav")
 
   mySidenav.classList.toggle("release")
 }
-
-/* Set the width of the side navigation to 0 */
-
-/* Set the color of the burger menu to white*/
+function scrollOff() {
+  const body = document.getElementById("body")
+  body.style.overflow = "hidden"
+}
+function scrollOn() {
+  const body = document.getElementById("body")
+  body.style.overflow = "visible"
+}
 function visibleCross() {
-  let dash = document.querySelector(".dash")
+  const dash = document.querySelector(".dash")
   dash.style.backgroundColor = "white"
 
-  let dash3 = document.querySelector(".dash-3")
+  const dash3 = document.querySelector(".dash-3")
   dash3.style.backgroundColor = "white"
 }
 function blackCross() {
-  let dash = document.querySelector(".dash")
+  const dash = document.querySelector(".dash")
   dash.style.backgroundColor = "black"
 
-  let dash3 = document.querySelector(".dash-3")
+  const dash3 = document.querySelector(".dash-3")
   dash3.style.backgroundColor = "black"
 }
 function addColorLogo() {
-  let logo = document.querySelector(".logo-text")
+  const logo = document.querySelector(".logo-text")
   logo.style.color = "white"
 }
 
 function removeColorLogo() {
-  let logo = document.querySelector(".logo-text")
+  const logo = document.querySelector(".logo-text")
   logo.style.color = "#c8d6df"
 }
 function menuHandler() {
-  let burger = document.getElementById("nav-icon4")
+  const burger = document.getElementById("nav-icon4")
 
   burger.addEventListener("click", () => {
     if (!burger.classList.contains("open")) {
       console.log("Aberto")
       burger.classList.add("open")
+
       addColorLogo()
       toggleNav()
-
       visibleCross()
+      scrollOff()
     } else if (burger.classList.contains("open")) {
       console.log("Fechado")
       burger.classList.remove("open")
+
       removeColorLogo()
       toggleNav()
       blackCross()
+      scrollOn()
     }
   })
 }
